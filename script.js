7fb59e1e52590500cbf7726ad42d8f4feac8bd51
Lucas L. Schimidt <70489851+LucasSch20@users.noreply.github.com>
2021-10-26 12:00:40 -0300

    v0.0.11

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var usuario = {
 
 function inputChangeMasc() {
     return inputGeneroFem.checked = false, usuario.genero = "masculino";
-    
+
 }
 function inputChangeFem() {
     return inputGeneroMasc.checked = false, usuario.genero = "feminino";
@@ -28,7 +28,8 @@ function loadLoadingForm() {
     let msgLoading = document.querySelector('#msgLoading');
     let msgInicial = document.querySelector('#msgInicial');
     let flexResultado = document.querySelector('.flexResultado');
-    let formResultado = document.querySelector('.formResultado');
+    let formResultadoIMC = document.querySelector('#formResultadoIMC');
+    let formResultadoPorc = document.querySelector('#formResultadoPorc')
     let msgFinal = document.querySelector('#msgFinal');
 
 
@@ -40,15 +41,16 @@ function loadLoadingForm() {
         msgLoading.style.display = 'block';
         msgInicial.style.display = 'none';
         loadingForm.style.opacity = '100';
-        setTimeout(function() {
+        setTimeout(function () {
             loadingForm.style.display = 'none';
             flexResultado.style.display = 'flex';
-            formResultado.style.display = 'block';
+            formResultadoIMC.style.display = 'block';
+            formResultadoPorc.style.display = 'block';
             formResultado.style.opacity = 100;
             msgLoading.style.display = 'none';
             msgFinal.style.display = 'block';
 
-        }, 5000);
+        }, 4000);
     }
 }
 
@@ -65,7 +67,7 @@ function checkInput() {
     console.log(usuario);
 
     //Teste pra ver se os inputs estão vazios ou não:
-    if (usuario.genero != "" && usuario.idade > 0 && usuario.peso > 0 && usuario.altura > 0 && usuario.gordura > 0 && usuario.atividade != "") {
+    if (usuario.genero != "" && usuario.idade > 0 && usuario.peso > 0 && usuario.altura > 0 && usuario.atividade != "") {
         return loadLoadingForm();
     } else {
         return alert('Preencha todos os campos.');
@@ -74,12 +76,35 @@ function checkInput() {
 
 //Função que calcula o IMC e retorna uma string:
 
-function imcCalculo() {
-    let altura = document.querySelector('');
-    let peso = document.querySelector('');
+function imcCalculo(altura, peso) {
+    let imcString = document.querySelector('#imcString');
+    let classificacaoString = document.querySelector('#classificacaoString');
 
-    let pesoAoQ = peso*peso;
-    let IMC = altura/pesoAoQ;
+    alturaMetro = altura / 100
+    let alturaAoQ = alturaMetro * alturaMetro;
 
-    return IMC;
+    let IMC = peso / alturaAoQ;
+    let classificacaoIMC;
+
+    //Função que classifica o IMC do usuário:
+
+    console.log(IMC);
+
+    if (IMC < 18) {
+        classificacaoIMC = 'Abaixo do peso';
+    } else if (18 < IMC && IMC < 24.9) {
+        classificacaoIMC = 'Normal';
+    } else if (25 < IMC && IMC < 29.9) {
+        classificacaoIMC = 'Acima do peso';
+    } else if (30 < IMC && IMC < 34.9) {
+        classificacaoIMC = 'Obesidade grau 1';
+    } else if (35 < IMC && IMC < 39.9) {
+        classificacaoIMC = 'Obesidade grau 2';
+    } else if (IMC > 40) {
+        classificacaoIMC = 'Obesidade grau 3';
+    }
+
+
+    console.log(classificacaoIMC);
+    return imcString.innerHTML = IMC.toString(), classificacaoString.innerHTML = classificacaoIMC;
 }
